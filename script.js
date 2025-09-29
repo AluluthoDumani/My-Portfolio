95e293a36ab059fa19ff8ccf4314e00098ba3090
AluluthoDumani <aluluthodumani99@gmail.com>
2025-09-29 10:58:49 +0200

    changes made

--- script.js
+++ script.js
@@ -59,3 +59,13 @@ document.querySelector(".contact-form").addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Message sent! 🚀");
 });
+// ========================
+// Hamburger Menu Toggle
+// ========================
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
